4bf7c03ad0331909cbedf7f97a97871c7eb39b74
Herlich Gonzalez <herlich@gmail.com>
2019-10-29 18:07:02 -0600

    servicios chat swift

--- sw.py
+++ sw.py
@@ -1,46 +1,72 @@
-import os,sys, psycopg2,flask,jsonify,request
+import os,sys, psycopg2,flask,jsonify,request,json
+from flask_cors import CORS
 
 #https://tecadmin.net/install-python-3-7-on-ubuntu-linuxmint/
+#https://bitlaunch.io/blog/create-a-bitcoin-node-with-ubuntu/
 
 conn_string = "host='localhost' dbname='swift' user='postgres' password='Guatemala1'"
 
 
-def hacequery(toMsg):
+def getMessages(toMsg):
+    from psycopg2.extras import RealDictCursor
     conn = psycopg2.connect(conn_string)
     list = []
-    cursor = conn.cursor()
-    cursor.execute("""select * from public."offlineChat" where tomsg = '"""+ toMsg +"'")
-    rows = cursor.fetchall()
-    rString = ""
-    for row in rows:
-        rString = "{\"msg\":\"" + row[0] +"\",\"tomsg\":\"" + row[1] +"\",\"sent\":\"" + str(row[2]) + "\",\"datetime\":\"" + str(row[3]) +"\"}"
-        list.append(rString)
-        print (rString)
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("""select msg, tomsg, sent, datetime from public."offlineChat" where tomsg = '"""+ toMsg +"'")
+    l = json.dumps(cursor.fetchall(),indent = 2)
     conn.close()
-    return list
+    borrarMensaje(toMsg)
+    return l
 
+def grabaMensaje(data):
+    #print(data)
+    #print(data['msg'])
+    #print(data['tomsg'])
+    #print(data['sent'])
+    #print(data['datetime'])
+    msg = str(data)#str(data['msg'])
+    tomsg = str(data['tomsg'])
+    sent = str(data['sent'])
+    datetime = str(data['datetime'])
+    
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    print(msg + " " + tomsg + " " + str(sent) + " " + datetime)
+    #query = """ insert into public."offlineChat" (msg,tomsg,sent,datetime) values ( '""" + data +"','" + tomsg +"'," + sent + ",'" +  datetime + "')"
+    q = """insert into public."offlineChat" (msg,tomsg,sent,datetime) values('"""+  msg.replace("'", "\"") +"','"+ tomsg +"','"+sent+"','" +datetime +"')"
+    #print(q)
+    cursor.execute(q)
+    conn.commit()
+    conn.close()
+
+def borrarMensaje(tomsg):
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    q = """ delete from public."offlineChat" where tomsg = '"""+ tomsg +"' "
+    #print(q)
+    cursor.execute(q)
+    conn.commit()
+    conn.close()
 
 
 
 from flask import Flask,request
-
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 #@app.route('/todo/api/v1.0/tasks', methods=['GET'])
 #def get_tasks():
 #    return jsonify({'tasks': tasks})
 
+
 #http://localhost:5000/api/get?username=herlich&password=pwd1
 @app.route('/api/get',methods=['GET'])
 def getMsgs():
-    #username = request.args.get('username')
-    #password = request.args.get('password')
     id = request.args.get('id')
     #msg = request.args.get('msg')
-    print(username +  " " + password)
-    #username = "herlich"
-    return """{\"msg\":\"ok\"}"""
+    print('get messages from: ' + id)
+    return getMessages(id)#"""{\"msg\":\"ok\"}"""
 
 
 #http://localhost:5000/api/get
@@ -56,11 +82,9 @@ def getMsgsJ():
 #http://localhost:5000/api/post
 @app.route('/api/post',methods=['POST'])
 def postMsg():
-    print('posting')
+    print('posting message')
     req_data = request.get_json()
-    msg = req_data['msg']
-    #hacequery("herlich@gmail.com")
-    print(msg)
+    grabaMensaje(req_data)
     return """{\"msg\":\"ok\"}"""#requests.status_codes._codes[200]
     
 
@@ -68,5 +92,5 @@ def postMsg():
 
 if __name__ == '__main__':
     #app.run(debug=True)
-    app.run(host='localhost', port=5000)
-#    hacequery("herlich@gmail.com")
+    print("starting chat service")
+    app.run(host='localhost', port=1500)

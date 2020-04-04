b3b8545b7c16191757b197e880ea86bf8fd54f42
herlich steven gonzalez zambrano <herlich@gmail.com>
2020-04-03 18:27:09 -0600

    added tweeter fetch tweets

--- tweeter.py
+++ tweeter.py
@@ -1,3 +1,27 @@
+'''
+API key:
+bCgXMvHfVr1f86jrcwJSIbfyU
+
+API secret key:
+UUYjGwM63n6UvZMPTmfZG0yUq5eDm2PE5747F0xht71pgr2g8v
+
+access token
+1432847989-W3qw9szAWWP0VxsPpEsvVZX6igJjrVJzUZrrgYY
+
+
+access token secret
+332CPmsifvklzEK33F99flSAde5zz71fCiaz4V1P6qYIs
+
+'''
+# estos valores se dan al habilitar el servicio de tweeter de la cuenta.
+cfg = { 
+  "consumer_key"        : "bCgXMvHfVr1f86jrcwJSIbfyU",
+  "consumer_secret"     : "UUYjGwM63n6UvZMPTmfZG0yUq5eDm2PE5747F0xht71pgr2g8v",
+  "access_token"        : "1432847989-W3qw9szAWWP0VxsPpEsvVZX6igJjrVJzUZrrgYY",
+  "access_token_secret" : "332CPmsifvklzEK33F99flSAde5zz71fCiaz4V1P6qYIs" 
+  }
+
+
 import tweepy
 #instalar el paquete de la siguiente forma:  pip install tweepi
 def get_api(cfg):
@@ -5,19 +29,26 @@ def get_api(cfg):
   auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
   return tweepy.API(auth)
 
-def main():
-  # estos valores se dan al habilitar el servicio de tweeter de la cuenta.
-  cfg = { 
-    "consumer_key"        : "VALUE",
-    "consumer_secret"     : "VALUE",
-    "access_token"        : "VALUE",
-    "access_token_secret" : "VALUE" 
-    }
-
+def sendTwitt():
   api = get_api(cfg)
   tweet = "Hello, world! msg from an py app"
   status = api.update_status(status=tweet) 
- 
+
+
+def getTweets(number):
+  api = get_api(cfg)
+  search_words = "#COVID2019"
+  date_since = "2020-02-16"
+  tweets = tweepy.Cursor(api.search,
+              q=search_words,
+              lang="en",
+              since=date_since).items(number)
+  # Iterate and print tweets
+  for tweet in tweets:
+      #print(tweet.text) 
+    print(tweet)
+
 
 if __name__ == "__main__":
-  main()
+  #sendTwitt()
+  getTweets(10)

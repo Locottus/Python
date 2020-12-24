669bcb84c4bccab2034a3c4e7006fabe9600e91d
herlich steven gonzalez zambrano <herlich@gmail.com>
2020-12-24 01:10:12 +0000

    added nginx and postgres to docker

--- machines/docker/docker-run.sh
+++ machines/docker/docker-run.sh
@@ -1,2 +1,5 @@
-docker rm postgresql
-docker run --name postgresql -e POSTGRES_PASSWORD=Guatemala1 -v /var/postgresql:/var/lib/postgresql/data -p 5432:5432 -d postgres
+sleep 15
+/usr/bin/docker rm postgresql
+/usr/bin/docker run --name postgresql -e POSTGRES_PASSWORD=Guatemala1 -v /var/postgresql:/var/lib/postgresql/data -p 5432:5432 -d postgres
+/usr/bin/docker rm nginxserver
+/usr/bin/docker run --name nginxserver  -v /var/www/html/:/usr/share/nginx/html/ -p 80:80  -d nginx
